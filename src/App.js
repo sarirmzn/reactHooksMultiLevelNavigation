a15897fde40b8b70f201702cbf74c6d5d16466cf
Ramazan SARI <15591100Rmz>
2021-10-10 03:56:31 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState([]);
+
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await fetch(
+                'https://61606b7dfaa03600179fba64.mockapi.io/data',
+            );
+            const json = await res.json();
+            setData(json);
+        };
+        fetchData();
+
+    }, [setData]);
+
+    const level1 = []
+    const level2 = []
+    const test = data.map((numbers) => {
+            if(numbers.USTKATEGORIID !== "0")
+                level1.push(numbers.USTKATEGORIID)
+            if(numbers.USTKATEGORIID !== "0")
+                level2.push(numbers)
+        }
+    );
+
+
+    level1.map((x) =>{
+
+        let abc = level2.filter(item => item.USTKATEGORIID === x)
+
+        let newMarkers = data.map(el => (
+            el.KNO === x ? {...el, "altKategori" : true ,"altKategoriIcerik": abc}: el
+        ))
+        let newarray = newMarkers.filter(element => element.USTKATEGORIID !== x);
+        setData(newarray);
+
+    })
+    console.log(data);
+
+
+    return (
+        <ul>
+            {data.map(item => (
+                <li key={item.KNO}>
+                    {item.KATEGORIADI}
+                    {item.altKategori ? <ul>
+                        {
+                            item.altKategoriIcerik.map(item => (
+                                <li key={item.KNO}>
+                                   {item.KATEGORIADI}
+                                    {item.altKategori ? <ul>
+                                        {
+                                            item.altKategoriIcerik.map(item => (
+                                                <li key={item.KNO}>
+                                                    {item.KATEGORIADI}
+                                                    {item.altKategori ? <ul>
+                                                        {
+                                                            item.altKategoriIcerik.map(item => (
+                                                                <li key={item.KNO}>
+                                                                    {item.KATEGORIADI}
+                                                                </li>
+                                                            ))
+                                                        }
+                                                    </ul> : false}
+                                                </li>
+                                            ))
+                                        }
+                                    </ul> : false}
+                                </li>
+                            ))
+                        }
+                    </ul> : false}
+                </li>
+            ))}
+        </ul>
+    );
 }
 
 export default App;
+
